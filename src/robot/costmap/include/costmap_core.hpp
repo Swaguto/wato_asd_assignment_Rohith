1a89edb54ef395a946a6ec181e221eee4097cb2f
Rohith Iruku <rohithiruku@be:36:8d:1a:73:8d.home>
2026-08-01 08:49:29 -0400

    changed header files

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -1,21 +1,24 @@
-#ifndef COSTMAP_CORE_HPP_
-#define COSTMAP_CORE_HPP_
+#ifndef COSTMAP_NODE_HPP_
+#define COSTMAP_NODE_HPP_
+ 
+#include "rclcpp/rclcpp.hpp" //ros client library
+#include "std_msgs/msg/string.hpp" //ros2 msgs
+ 
+#include "costmap_core.hpp" //costmap node
+ 
 
-#include "rclcpp/rclcpp.hpp"
-
-namespace robot
-{
-
-class CostmapCore {
+class CostmapNode : public rclcpp::Node {
   public:
-    // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
-    explicit CostmapCore(const rclcpp::Logger& logger);
-
+    CostmapNode();
+    
+    // Place callback function here
+    void publishMessage();
+ 
   private:
-    rclcpp::Logger logger_;
-
+    robot::CostmapCore costmap_;
+    // Place these constructs here
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
-
-}  
-
-#endif  
+ 
+#endif 
